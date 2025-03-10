10b3f711b8d93f2ccad18276a0fcdc272bcedae0
mahaddalkar-om <om.m@siltech.co.in>
2025-03-10 15:20:44 +0530

    organisation chart

--- app/Http/Controllers/organisationController.php
+++ app/Http/Controllers/organisationController.php
@@ -1,16 +1,26 @@
 <?php
-
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Auth;
 
-class organisationController extends Controller
+class OrganisationController extends Controller
 {
     public function showOrganisation()
     {
         $user = Auth::user(); // Get the logged-in user
+        
+        // Get the ID of the user with name "none" in the same organisation
+        $noneUser = DB::table('users')
+            ->where('name', 'none')
+            ->where('organisation_id', $user->organisation_id) // Assuming the logged-in user’s organisation
+            ->first();
+
+        // If we find the user with name "none", we get their ID
+        $noneUserId = $noneUser ? $noneUser->id : null;
+
+        // Fetch the employee details
         $employees = DB::table('emp_details')
             ->join('emp_contact_details', 'emp_details.user_id', '=', 'emp_contact_details.user_id')
             ->leftJoin('organisation_departments', 'emp_details.department', '=', 'organisation_departments.id')  // Join for department name
@@ -33,11 +43,6 @@ class organisationController extends Controller
             )
             ->get();
 
-        return view('user_view.organisation', compact('user', 'employees'));
-    }
-
-    public function displayEmployeeTree($parentId = null, $departmentColors)
-    {
-        // ...existing code...
+        return view('user_view.organisation', compact('user', 'employees', 'noneUserId'));
     }
 }
