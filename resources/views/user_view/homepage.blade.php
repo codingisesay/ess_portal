309a25421202e31602f1aebff28b2835f0dbb04a
mahaddalkar-om <om.m@siltech.co.in>
2025-03-18 11:58:39 +0530

    new buttons

--- resources/views/user_view/homepage.blade.php
+++ resources/views/user_view/homepage.blade.php
@@ -347,22 +347,40 @@ error_reporting(0);
                         <td>{{ $leave->leave_resion }}</td>
             
                         <!-- Approve Form -->
-                        <td>
+                        <!-- <td>
                             <form action="{{ route('leave_update_status', ['id' => $leave->leave_appliy_id, 'status' => 'Approved']) }}" method="POST">
                                 @csrf
                                 @method('PUT')
                                 <button type="submit" class="btn btn-success">Approve</button>
                             </form>
-                        </td>
+                        </td> -->
+                        <td>
+    <form action="{{ route('leave_update_status', ['id' => $leave->leave_appliy_id, 'status' => 'Approved']) }}" method="POST" style="display: inline;">
+        @csrf
+        @method('PUT')
+        <button type="submit" style="font-size: 24px;  border: none; background: none; cursor: pointer;">
+            ✅
+        </button>
+    </form>
+    </td>
+    <td>
+    <form action="{{ route('leave_update_status', ['id' => $leave->leave_appliy_id, 'status' => 'Reject']) }}" method="POST" style="display: inline;">
+        @csrf
+        @method('PUT')
+        <button type="submit" style="font-size: 24px; border: none; background: none; cursor: pointer;">
+            ❌
+        </button>
+    </form>
+</td>
             
                         <!-- Reject Form -->
-                        <td>
+                        <!-- <td>
                             <form action="{{ route('leave_update_status', ['id' => $leave->leave_appliy_id, 'status' => 'Reject']) }}" method="POST">
                                 @csrf
                                 @method('PUT')
                                 <button type="submit" class="btn btn-danger">Reject</button>
                             </form>
-                        </td>
+                        </td> -->
                     </tr>
                 @endforeach
             @endforeach
