cbc705e9e373194a2aff48f9628048df26f2d278
STPL\ankit <ankita.p@stpl.co.in>
2025-04-07 17:16:39 +0530

    UI chnages for sidebar and user page done.

--- resources/views/superadmin_view/superadmin_layout.blade.php
+++ resources/views/superadmin_view/superadmin_layout.blade.php
@@ -13,6 +13,8 @@ $name = Auth::guard('superadmin')->user()->name;
 <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Raleway">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/toastify-js/src/toastify.min.css">
+<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0&icon_names=settings" />
+<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0&icon_names=arrow_forward" />
     <script src="https://cdn.jsdelivr.net/npm/toastify-js"></script>
 
     <script src="{{ asset('user_end/js/toastify-notifications.js') }}"></script>
@@ -23,37 +25,61 @@ body {
     height: 100vh;
     margin: 0;
     font-family: 'Poppins', sans-serif;
-    background-color: #f6f6f6;
+    background-color: #F6F5F7;
     color: #333;
 }
-
+ 
+::-webkit-scrollbar {
+    width: 5px;
+}
+ 
+::-webkit-scrollbar-track {
+    -webkit-box-shadow: none  ;
+}
+ 
+::-webkit-scrollbar-thumb {
+  background-color: #EDEDEE; 
+  border-radius:30px
+}
 .sidebar {
-    width: 315px;
-    background-color: #F6F5F7;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    padding: 20px;
-    box-shadow: 2px 0 5px rgba(0, 0, 0, 0.1);
-    border-radius: 15px;
-    margin-left: -50px; /* Move sidebar slightly to the left */
+    width: 330px;
+    background-color: #FFF;
+    /* display: flex; */
+    /* flex-direction: column; */
+    /* align-items: center; */
+    padding: 10px;
+    box-shadow: 0px 6px 6px 0px #00000040;
+    /* border-radius: 15px; */
+    /* margin-left: -50px; */
     height: 100vh;
-    margin-top: -50px;
-    overflow: auto
-
+    /* margin-top: -50px; */
+    overflow: auto;
 }
-
+.sidebar::-webkit-scrollbar {
+  display: none;
+}
+.sidebar-header{display: flex;
+    align-items: center;
+    justify-content: space-between;
+    background: #F6F5F7;padding: 7px 20px;
+    border-radius:6px;
+    box-shadow: 0px 0px 4px 0px #00000040;
+}
 .logo img {
-    height: 50px;
-    margin-bottom: 20px;
+    height: auto;width: 45%; 
 }
-
+img.active-dd{ 
+    transform: rotate(90deg);
+}
 nav ul {
     list-style: none;
-    padding: 0;
+    padding: 20px 10px;
     width: 100%;
 }
-
+nav ul li ul{padding:10px 0 0 0;}
+nav ul li ul li{margin:0}
+nav ul li ul li a{padding: 7px 15px 7px 33px}
+nav ul li a.active{background: #8A33664D;box-shadow: 0px 0px 4px 0px #00000040;border-radius: 6px;}
 nav ul li {
     margin-bottom: 20px;
 }
@@ -62,7 +88,7 @@ nav ul li a {
     display: flex;
     align-items: center;
     gap: 10px;
-    color: #b1b1b1;
+    color: #656565; 
     text-decoration: none;
     transition: color 0.3s ease;
 }
@@ -72,7 +98,23 @@ nav ul li a:hover {
     color: #8A3366;
     font-weight: bold;
 }
-
+.nav-list{margin:0}
+.sidebar-content{height:75vh; overflow:auto;margin:5px 0}
+.sidebar-content ul li strong img{opacity: .58;}
+.sidebar-content ul li strong{ color:#656565; display : flex; justify-content: space-between;align-items: center; cursor: pointer;font-weight: 500; }
+.header-arrow{box-shadow: 0px 0px 4px 0px #00000040; background: #FFF;border-radius:50%; padding: 1px 
+}
+.sidebar-footer small{font-size:10px;}
+.sidebar-footer{
+    display: flex;
+    align-items: center;
+    gap: 10px;
+    margin-top: 20px;
+    padding: 10px;
+    background-color: #F6F5F7;
+    border-radius: 6px;
+    box-shadow: 0px 0px 4px 0px #00000040;
+}
 .sidebar-icons {
     margin-top: auto;
     display: flex;
@@ -142,92 +184,124 @@ nav ul li a:hover {
     var errorMessage = @json(session('error'));
 </script>
 <!-- Sidebar/menu -->
-<nav class="sidebar" id="mySidebar"><br>
+<nav class="sidebar" id="mySidebar">
   <div class="sidebar-header">
     <div class="logo">
     <img src="{{ asset('user_end/images/STPL Logo with TagLine HD Transparent.png') }}" alt="STPL Logo">
     </div>
-    
-  </div>
-  <hr>
+    <img src="{{ asset('user_end/images/arrow-right.svg') }}" class="header-arrow" alt="arrow">
+  </div> 
   <div class="sidebar-content">
-    <ul>
+    <ul class="nav-list">
         <!-- Organisation Configuration -->
         <li>
             <strong onclick="toggleDropdown('orgConfigDropdown', this)">
-                Organisation Configuration <span class="dropdown-arrow">&#9660;</span>
+            <strong> <span class="material-symbols-outlined">settings </span>&nbsp;   Organisation Configuration</strong>
+            <img src="{{ asset('user_end/images/arrow-right.svg') }}" alt="arrow"  class="dropdown-arrow"> 
             </strong>
             <ul id="orgConfigDropdown" style="display: none;">
-			    <li><a href="{{ route('create_user') }}" class="active"><img src="{{ asset('admin_end/images/man.png') }}" width="30" height="30" alt=""> Create User</a></li>
-                <li><a href="{{ route('create_branch_form') }}"><img src="{{ asset('admin_end/images/createbranch.png') }}" width="30" height="30" alt=""> Create Branch</a></li>
-                <li><a href="{{ route('create_department_form') }}"><img src="{{ asset('admin_end/images/corporation.png') }}" width="30" height="30" alt=""> Create Department</a></li>
-                <li><a href="{{ route('create_designation_form') }}"><img src="{{ asset('admin_end/images/createdesignation.png') }}" width="30" height="30" alt=""> Create Designation</a></li>
+			    <li><a href="{{ route('create_user') }}" class="active">  Create User</a></li>
+                <li><a href="{{ route('create_branch_form') }}"> Create Branch</a></li>
+                <li><a href="{{ route('create_department_form') }}">  Create Department</a></li>
+                <li><a href="{{ route('create_designation_form') }}">  Create Designation</a></li>
             </ul>
         </li>
 
         <!-- Policy Management -->
         <li>
             <strong onclick="toggleDropdown('policyManagementDropdown', this)">
-                Policy Management <span class="dropdown-arrow">&#9660;</span>
+            <strong> <span class="material-symbols-outlined">settings </span>&nbsp;   Policy Management </strong>
+                <img src="{{ asset('user_end/images/arrow-right.svg') }}" alt="arrow"  class="dropdown-arrow"> 
             </strong>
             <ul id="policyManagementDropdown" style="display: none;">
-                <li><a href="{{ route('create_policy_category') }}"><img src="{{ asset('admin_end/images/createdesignation.png') }}" width="30" height="30" alt=""> Create Policy Category</a></li>
-                <li><a href="{{ route('create_hr_policy') }}"><img src="{{ asset('admin_end/images/createdesignation.png') }}" width="30" height="30" alt=""> Create HR Policy</a></li>
-                <li><a href="{{ route('create_policy_time_slot') }}"><img src="{{ asset('admin_end/images/createdesignation.png') }}" width="30" height="30" alt=""> Leave Policy Slot</a></li>
-                <li><a href="{{ route('create_policy_type') }}"><img src="{{ asset('admin_end/images/createdesignation.png') }}" width="30" height="30" alt=""> Leave Policy Type</a></li>
-                <li><a href="{{ route('create_policy') }}"><img src="{{ asset('admin_end/images/createdesignation.png') }}" width="30" height="30" alt=""> Leave Policy Creation</a></li>
-                <li><a href="{{ route('employee_policy') }}"><img src="{{ asset('admin_end/images/createdesignation.png') }}" width="30" height="30" alt=""> Leave Emp Policy</a></li>
-                <li><a href="{{ route('process_leave_policy') }}"><img src="{{ asset('admin_end/images/createdesignation.png') }}" width="30" height="30" alt=""> Process Leave Cycle</a></li>
+                <li><a href="{{ route('create_policy_category') }}">  Create Policy Category</a></li>
+                <li><a href="{{ route('create_hr_policy') }}">  Create HR Policy</a></li>
+                <li><a href="{{ route('create_policy_time_slot') }}">  Leave Policy Slot</a></li>
+                <li><a href="{{ route('create_policy_type') }}">  Leave Policy Type</a></li>
+                <li><a href="{{ route('create_policy') }}">  Leave Policy Creation</a></li>
+                <li><a href="{{ route('employee_policy') }}">  Leave Emp Policy</a></li>
+                <li><a href="{{ route('process_leave_policy') }}">  Process Leave Cycle</a></li>
             </ul>
         </li>
 
         <!-- Salary Management -->
         <li>
             <strong onclick="toggleDropdown('salaryManagementDropdown', this)">
-                Salary Management <span class="dropdown-arrow">&#9660;</span>
+            <strong> <span class="material-symbols-outlined">settings </span>&nbsp;  Salary Management   </strong>
+               <img src="{{ asset('user_end/images/arrow-right.svg') }}" alt="arrow"  class="dropdown-arrow"> 
             </strong>
             <ul id="salaryManagementDropdown" style="display: none;">
-                <li><a href="{{ route('salary_template_form') }}"><img src="{{ asset('admin_end/images/createdesignation.png') }}" width="30" height="30" alt=""> Create Salary Templates</a></li>
-                <li><a href="{{ route('create_salary_components') }}"><img src="{{ asset('admin_end/images/createdesignation.png') }}" width="30" height="30" alt=""> Salary Template Components</a></li>
+                <li><a href="{{ route('salary_template_form') }}">  Create Salary Templates</a></li>
+                <li><a href="{{ route('create_salary_components') }}">  Salary Template Components</a></li>
             </ul>
         </li>
 
         <!-- Tax Management -->
         <li>
             <strong onclick="toggleDropdown('taxManagementDropdown', this)">
-                Tax Management <span class="dropdown-arrow">&#9660;</span>
+            <strong> <span class="material-symbols-outlined">settings </span>&nbsp;    Tax Management </strong>
+               <img src="{{ asset('user_end/images/arrow-right.svg') }}" alt="arrow"  class="dropdown-arrow"> 
             </strong>
             <ul id="taxManagementDropdown" style="display: none;">
-                <li><a href="{{ route('tax_cycle') }}"><img src="{{ asset('admin_end/images/createdesignation.png') }}" width="30" height="30" alt=""> Tax Cycle</a></li>
-                <li><a href="{{ route('taxes') }}"><img src="{{ asset('admin_end/images/createdesignation.png') }}" width="30" height="30" alt=""> Taxes</a></li>
+                <li><a href="{{ route('tax_cycle') }}">  Tax Cycle</a></li>
+                <li><a href="{{ route('taxes') }}">  Taxes</a></li>
             </ul>
         </li>
 
         <!-- Settings -->
         <li>
             <strong onclick="toggleDropdown('settingsDropdown', this)">
-                Settings <span class="dropdown-arrow">&#9660;</span>
+            <strong> <span class="material-symbols-outlined">settings </span>&nbsp; Settings  </strong>
+            <img src="{{ asset('user_end/images/arrow-right.svg') }}" alt="arrow"  class="dropdown-arrow"> 
             </strong>
             <ul id="settingsDropdown" style="display: none;">
-                <li><a href="{{ route('load_mail_config_form') }}"><img src="{{ asset('admin_end/images/createdesignation.png') }}" width="30" height="30" alt=""> Mail Settings</a></li>
+                <li><a href="{{ route('load_mail_config_form') }}">  Mail Settings</a></li>
             </ul>
         </li>
     </ul>
   </div>
+
+  <div class="sidebar-footer">
+        <img  src="{{ asset('admin_end/images/support.png') }}" alt="">
+        <div>
+        <strong>Need Help?</strong> <br>
+        <small>Go to Help Center 
+        <small class="material-symbols-outlined mt-2">arrow_forward</small> 
+        </small>
+       
+        </div>
+    </div>
 </nav>
 
 <script>
+    // function toggleDropdown(id, element) {
+    //     const dropdown = document.getElementById(id);
+    //     const arrow = element.querySelector('.dropdown-arrow');
+    //     if (dropdown.style.display === 'none') {
+    //         dropdown.style.display = 'block';
+    //         arrow.innerHTML = `<img class="active-dd" src="{{ asset('user_end/images/arrow-right.svg') }}" alt="arrow">`; // Up arrow
+    //     } else {
+    //         dropdown.style.display = 'none';
+    //         arrow.innerHTML = `<img class="active-dd" src="{{ asset('user_end/images/arrow-right.svg') }}" alt="arrow">`; // Down arrow
+    //     }
+    // }
+
     function toggleDropdown(id, element) {
-        const dropdown = document.getElementById(id);
-        const arrow = element.querySelector('.dropdown-arrow');
-        if (dropdown.style.display === 'none') {
-            dropdown.style.display = 'block';
-            arrow.innerHTML = '&#9650;'; // Up arrow
-        } else {
-            dropdown.style.display = 'none';
-            arrow.innerHTML = '&#9660;'; // Down arrow
-        }
+    const dropdown = document.getElementById(id);
+    const arrow = element.querySelector('.dropdown-arrow');
+
+    // Toggle the dropdown display
+    if (dropdown.style.display === 'none') {
+        dropdown.style.display = 'block';
+        // Rotate the arrow 90 degrees to point down (or up, depending on initial state)
+        arrow.style.transform = 'rotate(90deg)';
+    } else {
+        dropdown.style.display = 'none';
+        // Reset the rotation back to 0 degrees
+        arrow.style.transform = 'rotate(0deg)';
     }
+}
+
 </script>
 
   @yield('content')
