5b12ef4365b38c25fb0272c3547a7d153d3624a2
STPL\ankit <ankita.p@stpl.co.in>
2025-04-17 09:38:18 +0530

    employee details vertical card visuals completed.

--- resources/views/user_view/employment_details_top.blade.php
+++ resources/views/user_view/employment_details_top.blade.php
@@ -2,7 +2,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="{{ asset('/user_end/css/employment_data.css') }}">
-    {{-- <title>Employee Details</title> --}}
+    <!-- {{-- <title>Employee Details</title> --}} -->
     <link rel="icon" href="{{ asset('user_end/images/STPLLogo butterfly.png') }}" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
 </head>
@@ -11,14 +11,16 @@
             <?php 
             $profileimahe = session('profile_image');
             ?>
-            <img id="profile-image" src="{{ asset('storage/'.$profileimahe) }}" class="profile-image" alt="Profile Picture"> <br><br>
-            <h2 id="emp-name">{{ ucfirst($userDetails->name ?? '-') }}</h2>
+            <img id="profile-image" src="{{ asset('storage/'.$profileimahe) }}" class="profile-image" alt="Profile Picture"> <br>
+            <h4 id="emp-name">{{ ucfirst($userDetails->name ?? '-') }}</h4>
         </div>
-        <table class="job-title">
-            <tr><td>Designation</td> <td id="emp-designation">{{ ucfirst($userDetails->designation_name ?? '-') }}</td></tr>
-            <tr><td>Department</td> <td id="emp-department">{{ ucfirst($userDetails->department_name ?? '-') }}</td></tr>
-            <tr><td>Office</td> <td id="emp-city">{{ ucfirst($userDetails->branch_name ?? '-') }}</td></tr>
-            <tr><td>Reporting Manager</td> <td id="emp-manager">{{ ucfirst($userDetails->reporting_manager_name ?? '-') }}</td></tr>
-        </table>
+        <hr>
+        <p><strong>Designation</strong> <weak id="emp-designation">{{ ucfirst($userDetails->designation_name ?? '-') }}</weak></p>
+        <p><strong>Department</strong> <weak  id="emp-department">{{ ucfirst($userDetails->department_name ?? '-') }}</weak></p>
+        <p><strong>Office</strong> <weak id="emp-city">{{ ucfirst($userDetails->branch_name ?? '-') }}</weak></p>
+        <p><strong>Reporting Manager</strong> <weak  id="emp-manager">{{ ucfirst($userDetails->reporting_manager_name ?? '-') }}</weak></p>
+         
+
+
         </div> 
          
