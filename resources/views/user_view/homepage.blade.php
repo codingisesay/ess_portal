137c5be366bb29a14cdb4613b451715910267e85
Samidha <samidha.l@payvance.co.in>
2025-10-07 15:50:33 +0530

    General leave bar div from home page fixed start #3815 fixes #3815

--- resources/views/user_view/homepage.blade.php
+++ resources/views/user_view/homepage.blade.php
@@ -302,8 +302,8 @@ error_reporting(0);
                     @foreach($leaveUsage as $leave)
                         <li class='mb-2'>
                             <h6>{{ $leave[0] }}</h6> <!-- The first item is the leave type name -->
-                            <div class="progress-bar-containerr">
-                                <div class="progress-barr" style="width: {{ $leave[3] }}%; background-color: 
+                            <div class="progress-bar-containerr" width:100% !important>
+                                <div class="progress-barr" style="width: {{ $leave[3], 100 }}%; background-color: 
                                     @if($leave[3] <= 50) #4caf50
                                     @elseif($leave[3] <= 75) #ff9800
                                     @else #f44336
